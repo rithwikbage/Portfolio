0a33880d6c59c0b0100e89d038b539f2383de0c3
rithwikbage <rithwikbage@gmail.com>
2023-10-14 19:58:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+/*here targeting menu & icon elements
+& whenever clicked it will either add or remove the open class*/
+
+
+  
